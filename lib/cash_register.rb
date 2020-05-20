9fd1aa2ced7fc545cc6e47e56239b6b18b6ebe8c
Brian Matheny <bwmatheny@gmail.com>
2020-05-20 18:01:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,8 +8,10 @@ attr_accessor :total, :discount, :items, :last_transaction
   end
 
   def add_item(title, price, quantity=1)
-    @items << title[quantity]
     @total += price * quantity
+    quantity.times do
+      @items << title
+    end
   end
 
   def apply_discount
