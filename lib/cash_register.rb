f3297994ba4ad344714735a91350ba10382deabe
Brian Matheny <bwmatheny@gmail.com>
2020-05-20 17:51:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ attr_accessor :total, :discount, :items, :last_transaction
       @total = @total - (@total * @discount / 100)
       "After the discount, the total comes to $#{@total}."
     else
-      puts "There is no discount to apply."
+      "There is no discount to apply."
     end
   end
 
